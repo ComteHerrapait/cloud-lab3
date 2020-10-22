e1515a5c1fe219579014210f322239d3e8b3d234
Victor <victor.archambault@telecom-st-etienne.fr>
2020-10-22 15:04:23 +0200

    Yooooooooooooooooooooooooo

--- lab3_client.py
+++ lab3_client.py
@@ -1,5 +1,9 @@
 # Client
-def creerMessage():
+
+import boto3
+import time
+
+def createMessage():
 
     message=""
     for i in range(20):
@@ -11,4 +15,37 @@ def creerMessage():
             message+=str(e)
     
     return message
+
+def sendMessage():
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName = 'requestQueue')
+    #message = createMessage()
+    message = '1,5,22,33,9,0,-15,8,22,100,33,62,90,150'
+    response = queue.send_message(MessageBody = message)
+
+def readMessage():
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName = 'responseQueue')
+    messageList = []
+    messages_to_delete = []
+    for message in queue.receive_messages(MaxNumberOfMessages = 10):
+        print(message.body)
+        #TODO: log
+        messageList.append(message.body)
+        messages_to_delete.append({
+            'Id': message.message_id,
+            'ReceiptHandle': message.receipt_handle
+        })
+    if len(messages_to_delete) != 0:
+        delete_response = queue.delete_messages(
+            Entries=messages_to_delete)
+
+def main():
+
+    sendMessage()
+    time.sleep(6)
+    readMessage()
+
+print('yo')
+main()
 
--- lab3_ec2.py
+++ lab3_ec2.py
@@ -1,6 +1,8 @@
 # EC2
  
+import boto3
 import statistics
+from time import sleep
 # Parser pour récupérer le message sous forme d'une liste d'entiers
 def parser(message):
     liste=message.split(",")
@@ -27,3 +29,59 @@ def moyenne(listeEntiers):
 # Calcul de la mediane
 def mediane(listeEntiers):
     return statistics.median(listeEntiers)
+
+def process(message):
+    intList = parser(message)
+    minList = minimum(intList)
+    maxList = maximum(intList)
+    avgList = moyenne(intList)
+    medList = mediane(intList)
+
+    response = ("""The list minimum is : {} \nThe list maximum is : {} \nThe list average is : {} \nThe list median is : {}"""
+                ).format(minList, maxList, avgList, medList)
+    
+    return response
+
+def main():
+    sqs = boto3.resource('sqs')
+    requestQueue = sqs.get_queue_by_name(QueueName = 'requestQueue')
+    responseQueue = sqs.get_queue_by_name(QueueName = 'responseQueue')
+
+    file = open("log.txt", 'r+')
+    while True:
+        messageList = []
+        messages_to_delete = []
+        for message in requestQueue.receive_messages(MaxNumberOfMessages = 10):
+            print(message.body)
+            file.write(message.body)
+            messageList.append(message.body)
+            messages_to_delete.append({
+                'Id': message.message_id,
+                'ReceiptHandle': message.receipt_handle
+            })
+
+        # delete messages to remove them from SQS queue
+        if len(messages_to_delete) != 0:
+            delete_response = requestQueue.delete_messages(
+                Entries=messages_to_delete)
+        
+        else:
+            print('Empty')
+        
+        if len(messageList) != 0:
+            for message in messageList:
+                result = process(message)
+                responseQueue.send_message(MessageBody = result)
+
+        sleep(5)
+
+
+
+main()
+
+
+    
+
+
+
+
