a853a5eff574a1205f3a8861978fc648b0a0ce16
AriTortue <56339786+AriTortue@users.noreply.github.com>
2020-10-24 16:31:58 +0200

    Update numbers_client.py

--- numbers_client.py
+++ numbers_client.py
@@ -3,23 +3,16 @@
 import boto3
 from time import time
 from random import randint
-def createMessage():
-    message=""
-    for i in range(20):
-        print("Entrez un entier")
-        e=input()
-        if (i!=19):
-            message+=str(e)+","
-        else:
-            message+=str(e)
-    return message
 
 def inputMessage():
+""" takes no argument, returns a string with numbers separated by comma """
     message = ""
     while True:
-        i = input ("nombre(s) {} <- ".format(message))
+        i = input ("number(s) {} <- ".format(message))
+        # If the client stop entering numbers, the message is complete.
         if i == "":
             break
+        # Adds commas between integers in message
         else:
             if len(message) > 0 and message[-1] != ",":
                 message += ","
@@ -28,19 +21,32 @@ def inputMessage():
 
 
 def sendMessage(message):
+""" takes a string as argument, returns nothing, allows the client to send messages to the server """
+    # initiates a connection
     sqs = boto3.resource('sqs')
+    
+    # if existing, retrieves requestQueue
     queue = sqs.get_queue_by_name(QueueName = 'requestQueue')
+    
+    # sends message in the requestQueue
     queue.send_message(MessageBody = message)
 
 def readMessage():
-    waiting = "en attente de réponse"
+""" takes no argument, returns nothing, prints the response, allows the client to read responses of the server """
+    waiting = "Waiting for a response"
     response = ""
     print(waiting, end="\r")
-
+    
+    # seeks for the response of the server
     while response == "" :
+        # initiates a connection 
         sqs = boto3.resource('sqs')
+        
+        # if existing, retrieves responseQueue
         queue = sqs.get_queue_by_name(QueueName = 'responseQueue')
         messages_to_delete = []
+        
+        # gets the messages in the queue, adds it in the response variable and adds id and receipt_handle in the messages_to_delete list
         for message in queue.receive_messages(MaxNumberOfMessages = 10):
             response = message.body
             messages_to_delete.append({
@@ -48,57 +54,79 @@ def readMessage():
                 'ReceiptHandle': message.receipt_handle
             })
 
+        # deletes read messages from the queue
         if len(messages_to_delete) != 0:
             queue.delete_messages(Entries=messages_to_delete)
-        else: #waiting text
+            
+        # prints waiting text
+        else: 
             waiting += "."
             print(waiting, end="\r")
 
+    #prints response of the server
     print("\n\n",response,"\n")
 
 def purgeResponses():
+""" takes no argument, returns nothing, allows the client to purge the queue"""
     purged = 0
-
+    
+    # initiates a connection 
     sqs = boto3.resource('sqs')
+    
+    # if existing, retrieves responseQueue
     queue = sqs.get_queue_by_name(QueueName = 'responseQueue')
     messages_to_delete = []
+    
+    # gets the messages in the queue, adds id and receipt_handle in the messages_to_delete list
     for message in queue.receive_messages(MaxNumberOfMessages = 10):
         messages_to_delete.append({
             'Id': message.message_id,
             'ReceiptHandle': message.receipt_handle
         })
+        
+        # increments number of purged elements
         purged += 1
+     
+    # deletes messages from the queue
     if len(messages_to_delete) != 0:
         queue.delete_messages(Entries=messages_to_delete)
 
+    # prints the number of purged elements
     print("Purged {} messages.\n\n".format(purged))
 
 
 def main():
     while True:
-        print("1 - message préfait automatique")
-        print("2 - message aléatoire automatique")
-        print("3 - message saisi manuelement")
-        print("4 - purger les réponses de la queue")
-        print("0 - quitter\n\n")
+        
+        # Menu of the client application 
+        print("1 - Automatic pre made message")
+        print("2 - Automatic random message")
+        print("3 - Manually entered message")
+        print("4 - Purge queue")
+        print("0 - Exit\n\n")
         mode = input("choix : ")
 
+        # 0 - Exit option
         if mode == "0":
             break
 
-        elif mode == "1" : #message préfait automatique
+        # 1 - Automatic pre made message option
+        elif mode == "1" : 
             message = '1,5,22,33,9,0,-15,8,22,100,33,62,90,150,1,2,3'
 
-        elif mode == "2": #message aléatoire automatique
+        # 2 - Automatic random message option
+        elif mode == "2": 
             length = input("longueur de la liste : ")
             message = ""
             for i in range(int(length)) : 
                 message += str(randint(-100,100)) + str(",")
             message = message[:-1]
 
-        elif mode == "3": #message saisi manuelement
+        # 3 - Manually entered message option
+        elif mode == "3": 
             message = inputMessage()
         
+        # 4 - Purge queue option
         elif mode == "4":
             purgeResponses()
 
@@ -106,27 +134,34 @@ def main():
             print("invalid answer...")
             continue
 
-        if mode != "4": #n'envoie pas de message si purge selectionnée
+        # does not send message if purge message option has been selected
+        if mode != "4": 
             print("Envoi de : \n", message, "\n")
-
-            try :#ENVOI
+            
+            # sending
+            try :
                 chronoStart = time()
                 sendMessage(message)
+            
+            # error management
             except Exception as e:
-                print("### ERROR : impossible d'envoyer le message ###")
+                print("### ERROR : sending message has failed ###")
                 print(e, "\n\n")
                 continue
 
-            try :#RECEPTION
+            # reading
+            try :
                 readMessage()
                 chronoStop = time()
-                print("requete traitée en {:1.3f} secondes\n".format(chronoStop-chronoStart))
+                print("request treated within {:1.3f} secondes\n".format(chronoStop-chronoStart))
+            
+            # error management
             except Exception as e:
-                print("### ERROR : impossible de recevoir le message ###")
+                print("### ERROR : receiving message has failed ###")
                 print(e, "\n\n")
                 continue
 
-
+# running the previous code
 print('\n### PROGRAM INITIALIZED ###')
 purgeResponses()
 main()
